7b3be46c3b32994f1a010363dce990ab98fb3dc4
João Guilherme Couto <168004754+JnGuilherme07@users.noreply.github.com>
2024-05-13 08:41:17 -0300

    Login alterado

--- LOGIN/script.js
+++ LOGIN/script.js
@@ -1,10 +1,16 @@
-function btn(){
+ function btn(){
     var login = document.getElementById('login').value
     var senha = document.getElementById('senha').value
     var texto;
 
-    if(senha=== "1234" && login=== "1234"){
+    if(senha=== "1234" && login=== "USUARIO1"){
         texto= "Você está logado!"
+        window.location.href= "https://ge.globo.com/futebol/times/sao-paulo/"
+    }
+
+    if(senha=== "1234" && login=== "USUARIO2"){
+        texto= "Você está logado!"
+        window.location.href= "https://ge.globo.com/futebol/times/sao-paulo/"
     }
 
     else{
